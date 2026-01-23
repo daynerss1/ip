a803816a33e5e56ce7d46c3a9ce80f1466b7cd78
daynerss1 <e1375342@u.nus.edu>
2026-01-23 17:13:35 +0800

    Add exception logic

--- src/main/java/Barry.java
+++ src/main/java/Barry.java
@@ -40,6 +40,8 @@ public class Barry {
                     }
                 }
                 System.out.println(DIVIDER);
+            } else {
+                throw new BarryException("You need to specify 1 or more tasks to " + op + ".");
             }
         } else if (input.startsWith("todo ")) {
             String name = input.substring(5);
@@ -48,6 +50,9 @@ public class Barry {
             printAddedMessage(newToDo, userList.size());
         } else if (input.startsWith("deadline ")) {
             String[] parts = input.substring(9).split("/by", 2);
+            if (parts.length == 1) {
+                throw new BarryException("You need to input a date for the deadline of this task!");
+            }
             String name = parts[0].trim();
             String by = parts[1].trim();
             Deadline newDeadline = new Deadline(name, by);
@@ -55,27 +60,47 @@ public class Barry {
             printAddedMessage(newDeadline, userList.size());
         } else if (input.startsWith("event ")) {
             String[] parts = input.substring(6).split("/from", 2); // This is name + date/time
+            if (parts.length == 1) {
+                throw new BarryException("An event needs a starting time! Specify one by typing '/by <date/time>");
+            }
             String name = parts[0].trim();
             String[] times = parts[1].split("/to", 2);
+            if (times.length == 1) {
+                throw new BarryException("An event needs an end time! Specify one by typing '/to <date/time>");
+            }
             String start = times[0].trim();
             String end = times[1].trim();
             Event newEvent = new Event(name, start, end);
             userList.add(newEvent);
             printAddedMessage(newEvent, userList.size());
-        } else {
-            System.out.println(DIVIDER + "\nInvalid format: To add a new task, start the command with 'todo', 'deadline', or 'event'\n" + DIVIDER);
+        } else if (input.equals("event") || input.equals("download") || input.equals("todo")) {
+            String classifier = input.equals("event") ? "an ": "a ";
+            throw new BarryException("Oops! The description of " + classifier + input + " cannot be empty.");
+        }
+        else {
+            throw new BarryException("Invalid command: To add a new task, start the command with 'todo', 'deadline', or 'event'");
         }
     }
 
     public static void listTasks(ArrayList<Task> taskList) {
+        System.out.println(DIVIDER);
+        if (taskList.size() == 0) {
+            System.out.println("Your task list is currently empty.\n" + DIVIDER);
+            return;
+        }
         System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < taskList.size(); i++) {
             System.out.println((i + 1) + ". " + taskList.get(i).toString());
         }
+        System.out.println(DIVIDER);
     }
 
     public static void printAddedMessage(Task task, int size) {
         System.out.println(DIVIDER + "\nGot it. I've added this task:\n" + task.toString());
         System.out.println("Now you have " + size + " tasks in the list.\n" + DIVIDER);
+    }
+
+    public static void printErrorMessage(String msg) {
+        System.out.println(DIVIDER + "\n" + msg + "\n" + DIVIDER);
     }
 }
