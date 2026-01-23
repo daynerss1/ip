92881e644e3355c5a5b63c9eebeab5aeab2b7fc3
daynerss1 <e1375342@u.nus.edu>
2026-01-23 12:23:41 +0800

    Refactor user task list and list method

--- src/main/java/Barry.java
+++ src/main/java/Barry.java
@@ -3,24 +3,29 @@ import java.util.ArrayList;
 
 public class Barry {
     private static final String DIVIDER = "____________________________________________________________";
+    private static ArrayList<Task> userList = new ArrayList<>();
     public static void main(String[] args) {
-        ArrayList<String> userList = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         System.out.println(DIVIDER + "\nHello! I'm Barry\nWhat can I do for you?\n" + DIVIDER);
         String input = sc.nextLine();
         while (!input.equals("bye")) {
             if (input.equals("list")) {
                 System.out.println(DIVIDER);
-                for (int i = 0; i < userList.size(); i++) {
-                    System.out.println((i+1) + ". " + userList.get(i));
-                }
+                listTasks(userList);
                 System.out.println(DIVIDER);
             } else {
-                userList.add(input);
+                Task newTask = new Task(input);
+                userList.add(newTask);
                 System.out.println(DIVIDER + "\nadded: " + input + "\n" + DIVIDER);
             }
             input = sc.nextLine();
         }
         System.out.println(DIVIDER + "\nBye. Hope to see you again soon!\n" + DIVIDER);
+    }
+
+    public static void listTasks(ArrayList<Task> taskList) {
+        for (int i = 0; i < taskList.size(); i++) {
+            System.out.println((i+1) + ". " + taskList.get(i).toString());
+        }
     }
 }
