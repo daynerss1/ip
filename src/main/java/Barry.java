3f3cfdc5f60253a1a6fb60958e2201eb3d634141
daynerss1 <e1375342@u.nus.edu>
2026-01-23 12:58:36 +0800

    Added support for marking and unmarking tasks

--- src/main/java/Barry.java
+++ src/main/java/Barry.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 public class Barry {
     private static final String DIVIDER = "____________________________________________________________";
     private static ArrayList<Task> userList = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println(DIVIDER + "\nHello! I'm Barry\nWhat can I do for you?\n" + DIVIDER);
@@ -13,6 +14,24 @@ public class Barry {
                 System.out.println(DIVIDER);
                 listTasks(userList);
                 System.out.println(DIVIDER);
+            } else if (input.startsWith("mark") || input.startsWith("unmark")) {
+                String op = input.startsWith("mark") ? "mark" : "unmark";
+                String[] taskNums = input.split(" ");
+                if (taskNums.length > 1) {
+                    System.out.println(DIVIDER);
+                    if (op.equals("mark")) {
+                        for (int i = 1; i < taskNums.length; i++) {
+                            int number = Integer.parseInt(taskNums[i]);
+                            userList.get(number - 1).mark();
+                        }
+                    } else {
+                        for (int i = 1; i < taskNums.length; i++) {
+                            int number = Integer.parseInt(taskNums[i]);
+                            userList.get(number - 1).unmark();
+                        }
+                    }
+                    System.out.println(DIVIDER);
+                }
             } else {
                 Task newTask = new Task(input);
                 userList.add(newTask);
@@ -24,8 +43,9 @@ public class Barry {
     }
 
     public static void listTasks(ArrayList<Task> taskList) {
+        System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < taskList.size(); i++) {
-            System.out.println((i+1) + ". " + taskList.get(i).toString());
+            System.out.println((i + 1) + ". " + taskList.get(i).toString());
         }
     }
 }
