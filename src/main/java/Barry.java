e6822968db6ecfdb6a8bbfe388dfec35568ce1bf
daynerss1 <e1375342@u.nus.edu>
2026-01-23 11:36:49 +0800

    Echo commands by user

--- src/main/java/Barry.java
+++ src/main/java/Barry.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 public class Barry {
+    private static final String DIVIDER = "____________________________________________________________";
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Barry\nWhat can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(DIVIDER + "\nHello! I'm Barry\nWhat can I do for you?\n" + DIVIDER);
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(DIVIDER + "\n" + input + "\n" + DIVIDER);
+            input = sc.nextLine();
+        }
+        System.out.println(DIVIDER + "\nBye. Hope to see you again soon!\n" + DIVIDER);
     }
 }
