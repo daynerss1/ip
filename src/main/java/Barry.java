75ea5d058c4297b42d4478267a54a1216765fb89
daynerss1 <e1375342@u.nus.edu>
2026-01-23 11:55:05 +0800

    Add support for list

--- src/main/java/Barry.java
+++ src/main/java/Barry.java
@@ -1,13 +1,24 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Barry {
     private static final String DIVIDER = "____________________________________________________________";
     public static void main(String[] args) {
+        ArrayList<String> userList = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         System.out.println(DIVIDER + "\nHello! I'm Barry\nWhat can I do for you?\n" + DIVIDER);
         String input = sc.nextLine();
         while (!input.equals("bye")) {
-            System.out.println(DIVIDER + "\n" + input + "\n" + DIVIDER);
+            if (input.equals("list")) {
+                System.out.println(DIVIDER);
+                for (int i = 0; i < userList.size(); i++) {
+                    System.out.println((i+1) + ". " + userList.get(i));
+                }
+                System.out.println(DIVIDER);
+            } else {
+                userList.add(input);
+                System.out.println(DIVIDER + "\nadded: " + input + "\n" + DIVIDER);
+            }
             input = sc.nextLine();
         }
         System.out.println(DIVIDER + "\nBye. Hope to see you again soon!\n" + DIVIDER);
