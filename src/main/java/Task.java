b8d0f69dbe60c45ba4995c21122ce5ab17ab4f8e
daynerss1 <e1375342@u.nus.edu>
2026-01-23 12:10:34 +0800

    Add Task class
    Add support for marking a task as done or not done
    Add string representation for Task

---
+++ src/main/java/Task.java
@@ -0,0 +1,28 @@
+public class Task {
+    private String name;
+    private boolean done;
+
+    public Task(String name) {
+        this.name = name;
+        this.done = false;
+    }
+
+    public String markDone() {
+        System.out.println("Nice! I've marked this task as done:\n  ");
+        this.done = true;
+        return this.toString();
+
+    }
+
+    public String markNotDone() {
+        System.out.println("OK, I've marked this task as not done yet:\n  ");
+        this.done = false;
+        return this.toString();
+    }
+
+    @Override
+    public String toString() {
+        String completion = this.done ? "X" : " ";
+        return "[" + completion + "] " + this.name;
+    }
+}
